4a634d3df3f1baf6b315306500e8e715f2b3c2e7
rajkumar-kushwah <rajkumarkushwah3262@gmail.com>
2025-11-27 21:49:25 +0530

    Rename addAttendanceRoutes to workScheduleRoutes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import employeeRoutes from "./routes/employees.js";
 import salaryRoutes from "./routes/salary.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
-import addAttendanceRoutes  from './routes/attendanceAdd.js';
+import workScheduleRoutes  from './routes/attendanceAdd.js';
 
 
 
@@ -52,7 +52,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/salary", salaryRoutes);
 app.use("/api/attendance", attendanceRoutes);
-app.use("/api/add-attendance",  addAttendanceRoutes);
+app.use("/api/workschedules", workScheduleRoutes);
 app.use("/api", adminRoutes); 
 
 // Connect to MongoDB
